3208ec44180354393712d71d08b07648b27d1d2c
Works1990 <aguirre3590@gmail.com>
2025-03-17 12:19:17 -0500

    MO

--- backend/server.js
+++ backend/server.js
@@ -158,5 +158,6 @@ app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'index.html'));
 });
 
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
